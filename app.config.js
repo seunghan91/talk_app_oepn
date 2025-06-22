c61e90a27887a261112b00f20080bdf2ba4c4e1f
seunghan Kim <theqwe2000@naver.com>
2025-06-22 22:52:03 +0900

    chore: Bump iOS build number to 34

--- app.config.js
+++ app.config.js
@@ -23,7 +23,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.talkkapp.talkk",
-      buildNumber: "32"
+      buildNumber: "34"
     },
     android: {
       adaptiveIcon: {
